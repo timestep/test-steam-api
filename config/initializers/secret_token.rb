b7582e9785afb28d397371520c11ed7b5e7365ec
timestep <jbosshellokitty@gmail.com>
2013-07-31 11:12:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestSteamApi::Application.config.secret_key_base = 'f126ab127452a54213ceabbfb4ead1b58b16c4e8805353917b55007ec79ea065c96ed317ab528f9a08ef72c9699f49ff02fe8028a3e6ef3a1c2510caf6f33372'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestSteamApi::Application.config.session_store :cookie_store, key: '_test_steam_api_session'
